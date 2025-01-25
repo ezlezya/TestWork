046e94be9ccd4cd094e690f38b0990b05cc4ef92
Oleh <oleggolowatiy@gmail.com>
2025-01-25 23:46:27 +0200

    add try catch functionality

--- src/app/components/Appbar.jsx
+++ src/app/components/Appbar.jsx
@@ -1,10 +1,19 @@
+import { notFound } from 'next/navigation';
 import AppContainer from './AppContainer';
 
 export default async function AppBar() {
-  const response = await fetch('https://fakestoreapi.com/products/categories');
-  const data = await response.json();
+  try {
+    const response = await fetch('https://fakestoreapi.com/products/categories');
+    if (!response.ok) {
+      notFound()
+    }
+    const data = await response.json();
+    return (
+      <AppContainer data={data} />
+    );
+  } catch (err) {
+    console.error("We have an error while getting categories from api")
+    notFound()
+  }
 
-  return (
-    <AppContainer data={data} />
-  );
 }
--- src/app/components/MainContainer.jsx
+++ src/app/components/MainContainer.jsx
@@ -1,10 +1,18 @@
+import { notFound } from "next/navigation"
 import ProductsContainer from "../ProductsContainer"
 
 export default async function MainContainer() {
-    const response = await fetch(`https://fakestoreapi.com/products`)
-    const data = await response.json()
-
-    return (
-        <ProductsContainer data={data} />
-    )
+    try {
+        const response = await fetch(`https://fakestoreapi.com/products`)
+        if (!response.ok) {
+            notFound()
+        }
+        const data = await response.json()
+        return (
+            <ProductsContainer data={data} />
+        )
+    } catch (err) {
+        console.error("We have an error while getting products from api")
+        notFound()
+    }
 }
